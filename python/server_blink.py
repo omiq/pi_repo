832321f73829346253451d6a07da30bf43d4e084
OMIQ <chris@chrisg.com>
2015-10-02 17:52:00 -0600

    Robot control via web UI

--- python/server_blink.py
+++ python/server_blink.py
@@ -4,10 +4,13 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import pibrella
 
 import RPi.GPIO as GPIO
+import time
 from time import sleep
 
 # LED_PIN = 40 if BOARD
 LED_PIN = 21
+
+navigation = "<h1><a href=\"/l\">[L]</a> <a href=\"/f\">[F]</a> <a href=\"/r\">[R]</a></h1>"
 
 print "OK let's start!"
 
@@ -27,13 +30,13 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     blink()
-    return "Hello World!"
+    return navigation
 
 @app.route("/blink-blink")
 def blinkblink():
     blink()
     blink()
-    return "Blink. Blink"
+    return navigation
 
 @app.route("/beep")
 def beep():
@@ -41,7 +44,29 @@ def beep():
     sleep(1)
     pibrella.light.red.off()
     pibrella.buzzer.fail()
-    return "meep meep!"
+    return navigation
+
+@app.route("/f")
+def f():
+    pibrella.output.on()
+    time.sleep(1)
+    pibrella.output.off()
+    return navigation
+
+@app.route("/r")
+def r():
+    pibrella.output.e.on()
+    time.sleep(1)
+    pibrella.output.off()
+    return navigation
+
+@app.route("/l")
+def l():
+    pibrella.output.f.on()
+    time.sleep(1)
+    pibrella.output.off()
+    return navigation
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80, debug=True)
